88edfae4ef8dea01752358ed1a2f58a86add1fea
Julien Seguinot <julien.seguinot@natgeo.su.se>
2013-07-16 11:35:35 +0200

    Add precipitation variable

--- ptsgen.py
+++ ptsgen.py
@@ -4,7 +4,7 @@ from math import pi
 from netCDF4 import Dataset as NC
 import numpy as np
 
-def generate(output, tmin, tmax, n, amp):
+def generate(output, var, tmin, tmax, n, amp):
     """Generate NetCDF file"""
 
     # initialize netCDF file
@@ -12,11 +12,11 @@ def generate(output, tmin, tmax, n, amp):
     timedim = nc.createDimension('time', n)
     timevar = nc.createVariable('time', 'f4', 'time')
     timevar.units = 'years'
-    tempvar = nc.createVariable('delta_T', 'f4', 'time')
-    tempvar.units = 'K'
-    
+    tempvar = nc.createVariable('delta_%s' % var, 'f4', 'time')
+    tempvar.units = {'T': 'K', 'P': 'm year-1'}[var]
+
     # write data and close
-    timevar[:] = np.linspace(tmin, tmax, n) 
+    timevar[:] = np.linspace(tmin, tmax, n)
     tempvar[:] = (np.cos(2*pi*timevar[:]/(tmax-tmin)) - 1) * amp/2
     nc.close()
 
@@ -25,12 +25,15 @@ if __name__ == "__main__":
 
     # Argument parser
     parser = argparse.ArgumentParser(
-      description='''Cosine temperature offset time series generator''')
+      description='''Scalar offsets time series generator for PISM''')
     parser.add_argument('tmin', type=float, help='Start time in years')
     parser.add_argument('tmax', type=float, help='End time in years')
     parser.add_argument('dt',   type=float, help='Time interval in years')
-    parser.add_argument('amp',  type=float, help='Amplitude in Kelvin')
+    parser.add_argument('amp',  type=float, help='Amplitude in variable unit')
     parser.add_argument('-o', '--output', help='output file')
+    parser.add_argument('-v', '--variable',
+      choices=['T', 'P'], default='T',
+      help='Variable name (default: %(default)s)')
     args = parser.parse_args()
 
     # Parse arguments
@@ -38,10 +41,10 @@ if __name__ == "__main__":
     tmax = args.tmax
     dt   = args.dt
     amp  = args.amp
+    var  = args.variable
 
     # Output file name
     output = args.output or 'cos%gk-cool%02g.nc' % ((tmax-tmin)/1000, amp)
 
     # Generate netCDF file
-    generate(output, tmin, tmax, dt, amp)
-
+    generate(output, var, tmin, tmax, dt, amp)
