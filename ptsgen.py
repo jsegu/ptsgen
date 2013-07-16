37e3e89b521e181d426e649bfa99a7194fc60688
Julien Seguinot <julien.seguinot@natgeo.su.se>
2013-07-16 12:54:54 +0200

    Rework argument parser

--- ptsgen.py
+++ ptsgen.py
@@ -4,11 +4,11 @@ from math import pi
 from netCDF4 import Dataset as NC
 import numpy as np
 
-def generate(output, var, tmin, tmax, n, amp):
+def generate(tmin, tmax, orig, ampl, n=101, output=None, var='T'):
     """Generate NetCDF file"""
 
     # initialize netCDF file
-    nc = NC(output, 'w', format='NETCDF3_CLASSIC')
+    nc = NC(output or 'delta_%s.nc' % var, 'w', format='NETCDF3_CLASSIC')
     timedim = nc.createDimension('time', n)
     timevar = nc.createVariable('time', 'f4', 'time')
     timevar.units = 'years'
@@ -16,8 +16,9 @@ def generate(output, var, tmin, tmax, n, amp):
     tempvar.units = {'T': 'K', 'P': 'm year-1'}[var]
 
     # write data and close
-    timevar[:] = np.linspace(tmin, tmax, n)
-    tempvar[:] = (np.cos(2*pi*timevar[:]/(tmax-tmin)) - 1) * amp/2
+    t = np.linspace(0, 1, n)
+    timevar[:] = tmin + (tmax-tmin)*t
+    tempvar[:] = orig + ampl/2*(1-np.cos(2*pi*t))
     nc.close()
 
 if __name__ == "__main__":
@@ -28,23 +29,16 @@ if __name__ == "__main__":
       description='''Scalar offsets time series generator for PISM''')
     parser.add_argument('tmin', type=float, help='Start time in years')
     parser.add_argument('tmax', type=float, help='End time in years')
-    parser.add_argument('dt',   type=float, help='Time interval in years')
-    parser.add_argument('amp',  type=float, help='Amplitude in variable unit')
+    parser.add_argument('orig', type=float, help='Value at origin')
+    parser.add_argument('ampl', type=float, help='Amplitude')
+    parser.add_argument('-n', '--length', type=int, default=101,
+      help='Length of time series (default: %(default)s)')
     parser.add_argument('-o', '--output', help='output file')
     parser.add_argument('-v', '--variable',
       choices=['T', 'P'], default='T',
       help='Variable name (default: %(default)s)')
     args = parser.parse_args()
 
-    # Parse arguments
-    tmin = args.tmin
-    tmax = args.tmax
-    dt   = args.dt
-    amp  = args.amp
-    var  = args.variable
-
-    # Output file name
-    output = args.output or 'cos%gk-cool%02g.nc' % ((tmax-tmin)/1000, amp)
-
     # Generate netCDF file
-    generate(output, var, tmin, tmax, dt, amp)
+    generate(args.tmin, args.tmax, args.orig, args.ampl, args.length,
+      args.output, args.variable)
