fd4e33b6b19c03c5720ad845a3defa30bcb5168a
Julien Seguinot <julien.seguinot@natgeo.su.se>
2014-03-13 17:24:38 +0100

    Add GRIP ice core record
    Temperature reconstructed according to Johnsen et al. 2002, DOI 10.1034/j.1600-0889.47.issue5.9.x.

--- ptsgen.py
+++ ptsgen.py
@@ -28,15 +28,24 @@ def generate(func, tmin, tmax, orig, ampl, n=101, output=None, var='T'):
             tempvar[:] = orig + ampl/2*(1-np.cos(2*pi*t))
 
     # in case of a proxy record
-    elif func == 'epica':
-        if not os.path.isfile('epica.txt'):
-            url = ('ftp://ftp.ncdc.noaa.gov/pub/data/paleo/'
-                   'icecore/antarctica/epica_domec/edc3deuttemp2007.txt')
-            urllib.urlretrieve(url, 'epica.txt')
-        time, temp = np.genfromtxt('epica.txt', dtype='f4',
-                                   delimiter=(4, 13, 17, 13, 13),
-                                   skip_header=104, skip_footer=1,
-                                   usecols = (2, 4), unpack=True)
+    else:
+        if func == 'epica':
+            if not os.path.isfile('epica.txt'):
+                url = ('ftp://ftp.ncdc.noaa.gov/pub/data/paleo/'
+                       'icecore/antarctica/epica_domec/edc3deuttemp2007.txt')
+                urllib.urlretrieve(url, 'epica.txt')
+            time, temp = np.genfromtxt('epica.txt', dtype='f4',
+                                       delimiter=(4, 13, 17, 13, 13),
+                                       skip_header=104, skip_footer=1,
+                                       usecols = (2, 4), unpack=True)
+        elif func == 'grip':
+            if not os.path.isfile('grip.txt'):
+                url = ('ftp://ftp.ncdc.noaa.gov/pub/data/paleo/icecore/'
+                       'greenland/summit/grip/isotopes/gripd18o.txt')
+                urllib.urlretrieve(url, 'grip.txt')
+            time, d18o = np.genfromtxt('grip.txt', skip_header=37,
+                                       usecols=(2, 1), unpack=True)
+            temp = -11.88*(d18o-d18o[0]) - 0.1925*(d18o**2-d18o[0]**2)
         time = -time[::-1]
         temp = temp[::-1]
         avep = (-30e3 < time) * (time < -20e3)
@@ -56,7 +65,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description='''Scalar offsets time series generator for PISM''')
     parser.add_argument('func', type=str, help='Function to use',
-                        choices=['ramp', 'cos', 'epica'])
+                        choices=['ramp', 'cos', 'epica', 'grip'])
     parser.add_argument('tmin', type=float, help='Start time in years')
     parser.add_argument('tmax', type=float, help='End time in years')
     parser.add_argument('orig', type=float, help='Value at origin')
