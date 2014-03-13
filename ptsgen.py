0cc5bddb369c6d1654d2df28febd785e90b3cd14
Julien Seguinot <julien.seguinot@natgeo.su.se>
2014-03-13 15:57:19 +0100

    Comply with PEP 8 style guide

--- ptsgen.py
+++ ptsgen.py
@@ -4,6 +4,7 @@ from math import pi
 from netCDF4 import Dataset as NC
 import numpy as np
 
+
 def generate(func, tmin, tmax, orig, ampl, n=101, output=None, var='T'):
     """Generate NetCDF file"""
 
@@ -19,31 +20,32 @@ def generate(func, tmin, tmax, orig, ampl, n=101, output=None, var='T'):
     t = np.linspace(0, 1, n)
     timevar[:] = tmin + (tmax-tmin)*t
     if func == 'ramp':
-      tempvar[:] = orig + ampl*t
+        tempvar[:] = orig + ampl*t
     elif func == 'cos':
-      tempvar[:] = orig + ampl/2*(1-np.cos(2*pi*t))
+        tempvar[:] = orig + ampl/2*(1-np.cos(2*pi*t))
     nc.close()
+
 
 if __name__ == "__main__":
     import argparse
 
     # Argument parser
     parser = argparse.ArgumentParser(
-      description='''Scalar offsets time series generator for PISM''')
+        description='''Scalar offsets time series generator for PISM''')
     parser.add_argument('func', type=str, help='Function to use',
-      choices=['ramp', 'cos'])
+                        choices=['ramp', 'cos'])
     parser.add_argument('tmin', type=float, help='Start time in years')
     parser.add_argument('tmax', type=float, help='End time in years')
     parser.add_argument('orig', type=float, help='Value at origin')
     parser.add_argument('ampl', type=float, help='Amplitude')
     parser.add_argument('-n', '--length', type=int, default=101,
-      help='Length of time series (default: %(default)s)')
+                        help='Length of time series (default: %(default)s)')
     parser.add_argument('-o', '--output', help='output file')
     parser.add_argument('-v', '--variable',
-      choices=['T', 'P'], default='T',
-      help='Variable name (default: %(default)s)')
+                        choices=['T', 'P'], default='T',
+                        help='Variable name (default: %(default)s)')
     args = parser.parse_args()
 
     # Generate netCDF file
     generate(args.func, args.tmin, args.tmax, args.orig, args.ampl,
-      args.length, args.output, args.variable)
+             args.length, args.output, args.variable)
