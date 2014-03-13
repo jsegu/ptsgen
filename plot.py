3891c3f4ad5362355e7e757c089ad64af9aeaea9
Julien Seguinot <julien.seguinot@natgeo.su.se>
2014-03-13 17:27:43 +0100

    Add a minimal test
    Produce several files and use matplotlib to plot them on the screen.

---
+++ plot.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python2
+
+from netCDF4 import Dataset
+from matplotlib import pyplot as plt
+
+# plot
+for func in ['cos', 'ramp', 'grip', 'epica']:
+    nc = Dataset('%s.nc' % func)
+    time = nc.variables['time'][:]/1000.
+    temp = nc.variables['delta_T'][:]
+    nc.close()
+    plt.plot(time, temp)
+
+# add labels and save
+plt.xlim((-120, 0))
+plt.xlabel('time (ka)')
+plt.ylabel('delta_T (K)')
+plt.show()
---
+++ test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+for func in ramp cos epica grip
+do
+	python2 ptsgen.py $func -120000 0 0 -10 -o $func.nc
+done
+
+./plot.py
