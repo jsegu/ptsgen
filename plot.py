a4659adc02df8c664a7ed32ae9ce1c2a7efb1486
Julien Seguinot <julien.seguinot@natgeo.su.se>
2014-03-15 00:12:26 +0100

    Improve test plot readability

--- plot.py
+++ plot.py
@@ -1,20 +1,25 @@
 #!/usr/bin/env python2
+"""Plot test examples"""
 
 from netCDF4 import Dataset
 from matplotlib import pyplot as plt
 
+plt.rc('font', size=8)
+funclist = ['cos', 'ramp',
+            'lapaz21p', 'odp1012', 'odp1020',
+            'domefuji', 'epica',  'vostok', 'grip']
+
 # plot
-for func in ['cos', 'ramp',
-             'lapaz21p', 'odp1012', 'odp1020',
-             'domefuji', 'epica',  'vostok', 'grip']:
-    nc = Dataset('%s.nc' % func)
+for i, func in enumerate(funclist):
+    nc = Dataset(func + '.nc')
     time = nc.variables['time'][:]/1000.
     temp = nc.variables['delta_T'][:]
     nc.close()
-    plt.plot(time, temp)
+    plt.plot(time, temp-5*i, label=func)
 
 # add labels and save
 plt.xlim((-120, 0))
 plt.xlabel('time (ka)')
 plt.ylabel('delta_T (K)')
+plt.legend(loc='upper left')
 plt.show()
