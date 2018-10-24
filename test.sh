00819df859a946de07fa6270c8875f22214bbbf4
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2018-10-24 15:51:39 +0900

    Rename the project from ptsgen to pism-palseries.

--- test.sh
+++ test.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Copyright (c) 2014--2018, Julien Seguinot <seguinot@vaw.baug.ethz.ch>
+# GNU General Public License v3.0+ (https://www.gnu.org/licenses/gpl-3.0.txt)
 
 for func in ramp cos lapaz21p odp1012 odp1020 domefuji epica vostok ngrip grip guliya md012444
 do
